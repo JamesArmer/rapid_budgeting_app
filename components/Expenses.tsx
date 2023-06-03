27870cba7bdc12b1cb73c05c87d507d0dc8e143c
James Armer <jamesarmer2@gmail.com>
2023-06-03 12:50:08 +0100

    fix: expenses flex

--- components/Expenses.tsx
+++ components/Expenses.tsx
@@ -31,69 +31,75 @@ export default function Expenses({
       <div className="pl-3">
         <h3 className="font-bold text-2xl text-blue-main">Monthly Expenses</h3>
       </div>
-      <div className="flex">
-        <Stack direction="row" spacing={0}>
-          <Flex className="flex-1">
-            <GreyInput
-              name={rentInput}
-              title="Rent"
-              titleSize="sm"
-              defaultValue={
-                rent > 0 ? new Intl.NumberFormat().format(rent) : ""
-              }
-              placeholderText={new Intl.NumberFormat().format(rentPlaceholder)}
-              htmlSize={6}
-              onBlur={onBlur}
-            />
-          </Flex>
-          <Flex className="flex-1">
-            <GreyInput
-              name={billsInput}
-              title="Bills"
-              titleSize="sm"
-              defaultValue={
-                bills > 0 ? new Intl.NumberFormat().format(bills) : ""
-              }
-              placeholderText={new Intl.NumberFormat().format(billsPlaceholder)}
-              htmlSize={6}
-              onBlur={onBlur}
-            />
-          </Flex>
-        </Stack>
-      </div>
-      <div className="flex pt-2">
-        <Stack direction="row" spacing={0}>
-          <Flex className="flex-1">
-            <GreyInput
-              name={subscrInput}
-              title="Subscriptions"
-              titleSize="sm"
-              defaultValue={
-                subscr > 0 ? new Intl.NumberFormat().format(subscr) : ""
-              }
-              placeholderText={new Intl.NumberFormat().format(
-                subscrPlaceholder
-              )}
-              htmlSize={6}
-              onBlur={onBlur}
-            />
-          </Flex>
-          <Flex className="flex-1">
-            <GreyInput
-              name={savingsInput}
-              title="Savings"
-              titleSize="sm"
-              defaultValue={
-                savings > 0 ? new Intl.NumberFormat().format(savings) : ""
-              }
-              placeholderText={new Intl.NumberFormat().format(
-                savingsPlaceholder
-              )}
-              htmlSize={6}
-              onBlur={onBlur}
-            />
-          </Flex>
-        </Stack>
+      <div className="sm:flex">
+        <div className="flex-1">
+          <Stack direction="row" spacing={0}>
+            <Flex className="flex-1">
+              <GreyInput
+                name={rentInput}
+                title="Rent"
+                titleSize="sm"
+                defaultValue={
+                  rent > 0 ? new Intl.NumberFormat().format(rent) : ""
+                }
+                placeholderText={new Intl.NumberFormat().format(
+                  rentPlaceholder
+                )}
+                htmlSize={6}
+                onBlur={onBlur}
+              />
+            </Flex>
+            <Flex className="flex-1">
+              <GreyInput
+                name={billsInput}
+                title="Bills"
+                titleSize="sm"
+                defaultValue={
+                  bills > 0 ? new Intl.NumberFormat().format(bills) : ""
+                }
+                placeholderText={new Intl.NumberFormat().format(
+                  billsPlaceholder
+                )}
+                htmlSize={6}
+                onBlur={onBlur}
+              />
+            </Flex>
+          </Stack>
+        </div>
+        <div className="flex-1 pt-2 sm:pt-0">
+          <Stack direction="row" spacing={0}>
+            <Flex className="flex-1">
+              <GreyInput
+                name={subscrInput}
+                title="Subscriptions"
+                titleSize="sm"
+                defaultValue={
+                  subscr > 0 ? new Intl.NumberFormat().format(subscr) : ""
+                }
+                placeholderText={new Intl.NumberFormat().format(
+                  subscrPlaceholder
+                )}
+                htmlSize={6}
+                onBlur={onBlur}
+              />
+            </Flex>
+            <Flex className="flex-1">
+              <GreyInput
+                name={savingsInput}
+                title="Savings"
+                titleSize="sm"
+                defaultValue={
+                  savings > 0 ? new Intl.NumberFormat().format(savings) : ""
+                }
+                placeholderText={new Intl.NumberFormat().format(
+                  savingsPlaceholder
+                )}
+                htmlSize={6}
+                onBlur={onBlur}
+              />
+            </Flex>
+          </Stack>
+        </div>
       </div>
     </>
   );
